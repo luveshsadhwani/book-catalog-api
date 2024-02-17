2bb8341d017425cdd3623ba177614077059b61a4
Luvesh L Sadhwani <luveshsadhwani@gmail.com>
2024-02-17 20:33:04 +0800

    add authentication

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const apiRouter = require("./routes/api");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const session = require("express-session");
 
 const app = express();
 
@@ -13,12 +16,66 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.use(bodyParser.json());
 
+// configure passport strategy to use google auth 2.0
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "/auth/google/callback",
+        },
+        (accessToken, refreshToken, profile, cb) => {
+            console.log(cb);
+            return cb(null, profile);
+        }
+    )
+);
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+// Deserialize the user from the session
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+app.use(
+    session({
+        secret: "secret",
+        resave: false, // generally keep this to false
+        saveUninitialized: true,
+    })
+);
+
+// session management - once oauth verification is complete, we now need to store session data
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Mount router
 app.use("/api", apiRouter);
 
+app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    function (req, res) {
+        console.log(req);
+        // Successful authentication, redirect home.
+        res.redirect("/");
+    }
+);
+
 app.get("/", (req, res) => {
-    console.log("Hello!");
-    res.send();
+    if (req.isAuthenticated()) {
+        res.send(`Hello, ${req.user.displayName}!`);
+    } else {
+        res.send("Please log in.");
+    }
 });
 
 app.listen(PORT, () => {
