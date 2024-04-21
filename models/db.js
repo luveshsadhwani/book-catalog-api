04e88e39122284768c859f837cddd7b96f18e2b2
Luvesh L Sadhwani <luveshsadhwani@gmail.com>
2024-04-21 21:07:32 +0800

    Refactor book validation and API routing
    Simplified database methods by removing redundant book validations directly in db.js, moving validations into API routes. Introduced a BookData class with mock data for testing and consolidated book-related functions. Simplified API endpoints by leveraging BookData methods to interact with both mock and real data based on the environment. These changes improve code reusability, testability, and separation of concerns within our codebase.

--- models/db.js
+++ models/db.js
@@ -1,102 +1,46 @@
 const { pool } = require("../db/index");
 
-const isValidBook = (book) => {
-    if (!book.title || typeof book.title !== "string") {
-        const e = new Error("Book must have title!");
-        e.status = 400;
-        throw e;
-    }
-    if (!book.isbn || typeof book.isbn !== "string") {
-        const e = new Error("Book must have isbn!");
-        e.status = 400;
-        throw e;
-    }
-    if (book.isbn.length !== 10 && book.isbn.length !== 13) {
-        const e = new Error("ISBN must be either 10 or 13 characters long!");
-        e.status = 400;
-        throw e;
-    }
-    if (!book.author || typeof book.author !== "string") {
-        const e = new Error("Book must have author!");
-        e.status = 400;
-        throw e;
-    }
-    if (!book.genre || typeof book.genre !== "string") {
-        const e = new Error("Book must have genre!");
-        e.status = 400;
-        throw e;
-    }
-    if (!book.number_of_pages || isNaN(Number(book.number_of_pages))) {
-        const e = new Error("Number of pages must be a number!");
-        e.status = 400;
-        throw e;
-    }
-    if (!book.publication_year || isNaN(Number(book.publication_year))) {
-        const e = new Error("Publication year must be a number!");
-        e.status = 400;
-        throw e;
-    }
-    if (!book.publication_year > new Date().getFullYear) {
-        const e = new Error("Publication year cannot be in the future!");
-        e.status = 400;
-        throw e;
-    }
-    return true;
-};
-
-const isValidId = (id) => {
-    return id.length <= 13;
-};
-
+// Books
 const getBooks = async () => {
     const result = await pool.query("SELECT * FROM books");
     return result.rows;
 };
 
 const getBookById = async (id) => {
-    if (!id) {
-        throw new Error("Missing ID!");
-    }
     const result = await pool.query("SELECT * FROM books WHERE isbn = $1", [
         id,
     ]);
     return result.rows[0];
 };
 
-const addBook = async (book) => {
-    if (isValidBook(book)) {
-        const {
-            title,
-            isbn,
-            author,
-            genre,
-            number_of_pages,
-            publication_year,
-        } = book;
-        const result = await pool.query(
-            "INSERT INTO books (title, isbn, author, genre, number_of_pages, publication_year) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-            [title, isbn, author, genre, number_of_pages, publication_year]
-        );
-        return result.rows[0].isbn;
-    }
+const addBook = async ({
+    title,
+    isbn,
+    author,
+    genre,
+    number_of_pages,
+    publication_year,
+}) => {
+    const result = await pool.query(
+        "INSERT INTO books (title, isbn, author, genre, number_of_pages, publication_year) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+        [title, isbn, author, genre, number_of_pages, publication_year]
+    );
+    return result.rows[0].isbn;
 };
 
-const updateBook = async (bookToUpdate) => {
-    if (isValidBook(bookToUpdate)) {
-        const {
-            title,
-            isbn,
-            author,
-            genre,
-            number_of_pages,
-            publication_year,
-        } = bookToUpdate;
-        const result = await pool.query(
-            "UPDATE books SET title = $1, author = $2, genre = $3, number_of_pages = $4, publication_year = $5 WHERE isbn = $6 RETURNING *",
-            [title, author, genre, number_of_pages, publication_year, isbn]
-        );
-        return result.rows[0].isbn;
-    }
+const updateBook = async ({
+    title,
+    isbn,
+    author,
+    genre,
+    number_of_pages,
+    publication_year,
+}) => {
+    const result = await pool.query(
+        "UPDATE books SET title = $1, author = $2, genre = $3, number_of_pages = $4, publication_year = $5 WHERE isbn = $6 RETURNING *",
+        [title, author, genre, number_of_pages, publication_year, isbn]
+    );
+    return result.rows[0].isbn;
 };
 
 const deleteBook = async (id) => {
@@ -104,11 +48,85 @@ const deleteBook = async (id) => {
     return id;
 };
 
+const BOOKS_MOCK = [
+    {
+        title: "Hello",
+        isbn: "123456789",
+        genre: "Thriller",
+        number_of_pages: 300,
+        publication_year: 2020,
+    },
+    {
+        title: "World",
+        isbn: "123456788",
+        genre: "Action",
+        number_of_pages: 250,
+        publication_year: 2010,
+    },
+];
+
+const getBooksFromMock = () => {
+    return BOOKS_MOCK;
+};
+
+const getBookByIdFromMock = (id) => {
+    return BOOKS_MOCK.find((book) => book.isbn === id) || undefined;
+};
+
+const addBookToMock = ({
+    title,
+    isbn,
+    author,
+    genre,
+    number_of_pages,
+    publication_year,
+}) => {
+    BOOKS_MOCK.push({
+        title,
+        isbn,
+        author,
+        genre,
+        number_of_pages,
+        publication_year,
+    });
+    return isbn;
+};
+
+const updateBookFromMock = ({
+    title,
+    isbn,
+    author,
+    genre,
+    number_of_pages,
+    publication_year,
+}) => {
+    const index = BOOKS_MOCK.findIndex((book) => book.isbn === isbn);
+    BOOKS_MOCK[index] = {
+        title,
+        isbn,
+        author,
+        genre,
+        number_of_pages,
+        publication_year,
+    };
+    return isbn;
+};
+
+const deleteBookFromMock = (id) => {
+    const index = BOOKS_MOCK.findIndex((book) => book.isbn === id);
+    BOOKS_MOCK.splice(index, 1);
+    return id;
+};
+
 module.exports = {
     getBooks,
+    getBookById,
     addBook,
     updateBook,
     deleteBook,
-    isValidId,
-    getBookById,
+    getBooksFromMock,
+    getBookByIdFromMock,
+    addBookToMock,
+    updateBookFromMock,
+    deleteBookFromMock,
 };
--- routes/api.js
+++ routes/api.js
@@ -5,22 +5,27 @@ const {
     addBook,
     updateBook,
     deleteBook,
-    isValidId,
     getBookById,
+    getBooksFromMock,
+    getBookByIdFromMock,
+    addBookToMock,
+    updateBookFromMock,
+    deleteBookFromMock,
 } = require("../models/db");
+
+const Books = new BookData();
 
 apiRouter.param("id", (req, res, next, id) => {
     if (!isValidId(id)) {
         res.status(400).send(`Please pass valid book ISBN!`);
     } else {
-        req.bookId = id;
         next();
     }
 });
 
 apiRouter.get("/books", async (req, res) => {
     try {
-        const books = await getBooks();
+        const books = await Books._getBooks();
         res.status(200).json(books);
     } catch (err) {
         console.log(err);
@@ -34,8 +39,7 @@ apiRouter.get("/books", async (req, res) => {
 
 apiRouter.get("/books/:id", async (req, res) => {
     try {
-        const id = req.bookId;
-        const book = await getBookById(id);
+        const book = await Books._getBookById(req.params.id);
         if (!book) {
             const e = new Error(`Book with id ${id} not found`);
             e.status = 404;
@@ -54,9 +58,11 @@ apiRouter.get("/books/:id", async (req, res) => {
 
 apiRouter.post("/books", async (req, res) => {
     try {
-        const body = req.body;
-        const newBookIsbn = await addBook(body);
-        res.status(201).send(`Book added with ISBN ${newBookIsbn}`);
+        const bookToUpdate = req.body;
+        if (isValidBook(bookToUpdate)) {
+            const newBookIsbn = await Books._addBook(bookToUpdate);
+            res.status(201).send(`Book added with ISBN ${newBookIsbn}`);
+        }
     } catch (err) {
         console.log(err);
         if (err.status) {
@@ -69,10 +75,11 @@ apiRouter.post("/books", async (req, res) => {
 
 apiRouter.put("/book/:id", async (req, res) => {
     try {
-        const body = req.body;
-        body.id = req.bookId;
-        const updatedBookId = await updateBook(body);
-        res.status(200).send(`Book modified with ISBN ${updatedBookId}`);
+        const bookToUpdate = req.body;
+        if (isValidBook(bookToUpdate)) {
+            const updatedBookId = await Books._updateBook(bookToUpdate);
+            res.status(200).send(`Book modified with ISBN ${updatedBookId}`);
+        }
     } catch (err) {
         console.log(err);
         if (err.status) {
@@ -85,7 +92,7 @@ apiRouter.put("/book/:id", async (req, res) => {
 
 apiRouter.delete("/book/:id", async (req, res) => {
     try {
-        const deletedBookId = await deleteBook(req.bookId);
+        const deletedBookId = await Books._deleteBook(req.params.id);
         res.status(200).send(`Book deleted with ISBN ${deletedBookId}`);
     } catch (err) {
         console.log(err);
@@ -96,5 +103,71 @@ apiRouter.delete("/book/:id", async (req, res) => {
         }
     }
 });
+
+const isValidBook = (book) => {
+    if (!book.title || typeof book.title !== "string") {
+        const e = new Error("Book must have title!");
+        e.status = 400;
+        throw e;
+    }
+    if (!book.isbn || typeof book.isbn !== "string") {
+        const e = new Error("Book must have isbn!");
+        e.status = 400;
+        throw e;
+    }
+    if (book.isbn.length !== 10 && book.isbn.length !== 13) {
+        const e = new Error("ISBN must be either 10 or 13 characters long!");
+        e.status = 400;
+        throw e;
+    }
+    if (!book.author || typeof book.author !== "string") {
+        const e = new Error("Book must have author!");
+        e.status = 400;
+        throw e;
+    }
+    if (!book.genre || typeof book.genre !== "string") {
+        const e = new Error("Book must have genre!");
+        e.status = 400;
+        throw e;
+    }
+    if (!book.number_of_pages || isNaN(Number(book.number_of_pages))) {
+        const e = new Error("Number of pages must be a number!");
+        e.status = 400;
+        throw e;
+    }
+    if (!book.publication_year || isNaN(Number(book.publication_year))) {
+        const e = new Error("Publication year must be a number!");
+        e.status = 400;
+        throw e;
+    }
+    if (!book.publication_year > new Date().getFullYear) {
+        const e = new Error("Publication year cannot be in the future!");
+        e.status = 400;
+        throw e;
+    }
+    return true;
+};
+
+const isValidId = (id) => {
+    return id.length <= 13;
+};
+
+class BookData {
+    constructor(type = "mock") {
+        if (type === "mock") {
+            this._getBooks = getBooksFromMock;
+            this._getBookById = getBookByIdFromMock;
+            this._addBook = addBookToMock;
+            this._updateBook = updateBookFromMock;
+            this._deleteBook = deleteBookFromMock;
+        } else {
+            this._getBooks = getBooks;
+            this._getBookById = getBookById;
+            this._addBook = addBook;
+            this._updateBook = updateBook;
+            this._deleteBook = deleteBook;
+        }
+    }
+}
 
 module.exports = apiRouter;
